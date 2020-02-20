a9d9bf1bc68c2e9c350a7de75af9caa70d0e7b4a
Daniel Pirvoaica <61199768+ThreesomeProject@users.noreply.github.com>
2020-02-20 18:40:55 +0200

    SpaceShipp added
    Game object added + stayIn + Movememnt. Initial version

---
+++ SpaceInvaders/Assets/scripts/SpaceShip_move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip_move : MonoBehaviour
+{
+
+    private float deltaX, deltaY;
+    private Rigidbody2D rigidBdy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBdy = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            switch(touch.phase)
+            {
+                case TouchPhase.Began:
+                    deltaX = touchPos.x - transform.position.x;
+                    deltaY = touchPos.y - transform.position.y;
+                    break;
+
+                case TouchPhase.Moved:
+                    rigidBdy.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+                   // rigidBdy.MovePosition(new Vector2(Screen.currentResolution.width, Screen.currentResolution.height));
+                    break;
+
+                case TouchPhase.Ended:
+                    rigidBdy.velocity = Vector2.zero;
+                    break;
+            }
+            Debug.Log(touchPos.x+"  " +touchPos.y);
+        }
+    }
+}
---
+++ SpaceInvaders/Assets/scripts/stayIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stayIn : MonoBehaviour
+{
+    float horizontalMin, horizontalMax;
+    float verticalMin, verticalMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera camera = Camera.main;
+        float halfHeight = camera.orthographicSize;
+        float halfWidth = camera.aspect * halfHeight;
+
+        horizontalMin = -halfWidth;
+        horizontalMax = halfWidth;
+        verticalMin = -halfHeight;
+        verticalMax = halfHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 0, 2*horizontalMax),
+     Mathf.Clamp(transform.position.y, 0, 2*verticalMax),
+     transform.position.z);
+    }
+}
