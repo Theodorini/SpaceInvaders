d12d1cf66be38c5ebaaecc2ca00cb0263b5c4b11
Daniel Pirvoaica <61199768+ThreesomeProject@users.noreply.github.com>
2020-03-12 15:46:34 +0200

    Minor Changes to ability button

--- SpaceInvaders/Assets/scripts/SpaceShip.cs
+++ SpaceInvaders/Assets/scripts/SpaceShip.cs
@@ -22,11 +22,10 @@ public class SpaceShip : MonoBehaviour
         {
             // The screen has been touched so store the touch
             Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+            if ((touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) && touchPosition.y>Screen.height/10)
             {
                 // If the finger is on the screen, move the object smoothly to the touch position
-                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
                 touchPosition.z = 0f;
                 transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime * moveSpeed);
             }
